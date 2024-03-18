5bd6ea4caaa4e6e6ffdc621eb2d756efa5347de2
xxdanielngoxx <danielngo1998@gmail.com>
2024-03-19 01:17:30 +0700

    implement user login flow

--- app/routes/_layout.login.tsx
+++ app/routes/_layout.login.tsx
@@ -1,5 +1,57 @@
+import type { ActionFunctionArgs } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
+import { useActionData, useNavigation } from '@remix-run/react';
+
+import { Button } from '~/components/buttons';
+import { Card } from '~/components/containers';
+import { Form, Input } from '~/components/forms';
 import { H1 } from '~/components/headings';
+import { InlineError } from '~/components/texts';
+import { createUserSession, loginUser } from '~/modules/session/session.server';
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const { email, password } = Object.fromEntries(formData);
+
+  if (!email || !password) {
+    return json({
+      error: 'Please fill out all fields.',
+    });
+  }
+
+  if (typeof email !== 'string' || typeof password !== 'string') {
+    throw new Error('Invalid form data.');
+  }
+
+  try {
+    const user = await loginUser({ email, password });
+    return redirect('/dashboard', {
+      headers: await createUserSession(user),
+    });
+  } catch (error: any) {
+    return json({
+      error: error?.message ?? 'Something went wrong.',
+    });
+  }
+}
 
 export default function Component() {
-  return <H1>Login!</H1>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state !== 'idle' && navigation.formAction === '/login';
+
+  const actionData = useActionData<typeof action>();
+
+  return (
+    <Card>
+      <Form method="POST" action="/login">
+        <H1>Sign Up</H1>
+        <Input label="Email:" name="email" required />
+        <Input label="Password:" name="password" type="password" required />
+        <Button disabled={isSubmitting} type="submit" isPrimary>
+          {isSubmitting ? 'Logging you up...' : 'Log in!'}
+        </Button>
+        {actionData?.error && <InlineError aria-live="assertive">{actionData.error}</InlineError>}
+      </Form>
+    </Card>
+  );
 }
